e885ae118634e4bc5143bd8931cb929b36e4d26f
Savio Bajpai <joelbajpai7@gmail.com>
2021-07-11 14:14:03 +0530

    Add a testcase and a function in the UserManager to create a superuser

--- app/core/models.py
+++ app/core/models.py
@@ -16,6 +16,15 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password):
+        super_user = self.create_user(email, password)
+
+        super_user.is_staff = True
+        super_user.is_superuser = True
+        super_user.save(using=self._db)
+
+        return super_user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports email id instead of username"""
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,22 +1,22 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+EMAIL = 'test@test.com'
+PASSWORD = 'test123'
+
 
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
 
-        email = 'test@test.com'
-        password = 'test123'
-
         user = get_user_model().objects.create_user(
-            email=email,
-            password=password
+            email=EMAIL,
+            password=PASSWORD
         )
 
-        self.assertEqual(user.email, email)
-        self.assertTrue(user.check_password(password))
+        self.assertEqual(user.email, EMAIL)
+        self.assertTrue(user.check_password(PASSWORD))
 
     def test_new_user_email_normailized(self):
         """Test the email for the new user to be normalized"""
@@ -34,4 +34,15 @@ class ModelTests(TestCase):
         """Test creating a user with no email raises an error"""
 
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(None, 'test123')
+            get_user_model().objects.create_user(None, PASSWORD)
+
+    def test_create_supseruser(self):
+        """Test creating a superuser"""
+
+        user = get_user_model().objects.create_superuser(
+            email=EMAIL,
+            password=PASSWORD
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
